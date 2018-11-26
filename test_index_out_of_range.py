499d06a1eaab1566c5b8f056152a87f9c37516a1
steven <chenglin@ont.com>
2018-11-26 14:41:20 +0800

    add test_index_out_of_range.py

---
+++ test_index_out_of_range.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import ontology.builtins
+
+def Main():
+    a = 20
+    l = makelist(a)
+    print(a)
+    b = l[20]
+    print(b)
+    
+def makelist(maxl):
+    a = []
+    for i in range(0,maxl):
+        a.append(i)
+    return a
